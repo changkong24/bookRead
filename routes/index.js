a3433c3f61deeae758926cde78f1ad7905faff4c
changkong24 <2429669674@qq.com>
2016-09-05 22:33:58 +0800

    add htmls

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/book', function(req, res, next) {
+  res.render('book', { title: 'Express' });
+});
+
+module.exports = router;
